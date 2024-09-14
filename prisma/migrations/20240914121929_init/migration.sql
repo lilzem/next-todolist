aeb08479c399a1ac57c04d3fa0f3177a0b79958a
lilzem <dmytrynskiy74@gmail.com>
2024-09-14 18:47:23 +0300

    build fixing

--- prisma/migrations/20240914121929_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "text" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
